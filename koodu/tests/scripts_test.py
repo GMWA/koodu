eaae0c0162908cbb97ab9214939c6c568978d097
Alex Mboutchouang <mboutchouangalex@gmail.com>
2022-08-15 01:06:51 +0200

    create test files

---
+++ koodu/tests/scripts_test.py
@@ -0,0 +1,12 @@
+import unittest
+from koodu.generator import Generator, File
+from koodu.scripts.generate import generate
+from koodu.scripts.checks import check_templates
+
+
+class TestScripts(unittest.TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self):
+        pass
